24f74c1f8218020e645b2733a4d62f239ca14a2e
Harr1l <harri.laine@gmail.com>
2014-11-11 09:22:24 +0100

    Possibility to add user to an exercise

--- onlinepython/web.py
+++ onlinepython/web.py
@@ -8,6 +8,7 @@ Created on Sun Oct  5 20:47:49 2014
 #Python modules
 import cherrypy
 import db
+import os
 from config import ADMIN_USERNAME, ADMIN_PASSWORD
 from jinja2 import Environment, FileSystemLoader
 
@@ -62,8 +63,50 @@ class AdminIndex(object):
     @cherrypy.expose
     def console(self):
         self.verify_session()
+        exercise_list = db.get_exercises()
+        user_list = db.get_users()
+        tmpl = env.get_template('admin_console.html')
+        return tmpl.render(exercises=exercise_list, users=user_list)
 
-        return "OK"
+    @cherrypy.expose(alias='createExercise')
+    def create_exercise(self, description, timeLimit, output):
+        self.verify_session()
+        db.create_exercise(description, timeLimit, output)
+        raise cherrypy.HTTPRedirect("/admin/console")
+        
+    @cherrypy.expose(alias='createUser')
+    def create_user(self, username, password):
+        self.verify_session()
+        db.create_user(username, password)
+        raise cherrypy.HTTPRedirect("/admin/console")
+        
+    @cherrypy.expose
+    def edit_exercise(self, id):
+        self.verify_session()
+        data = db.get_exercises(id)
+        tmpl = env.get_template('admin_edit_exercise.html')
+        return tmpl.render(description=data.description, timelimit=data.time_limit, output=data.expected_output, id=data.id)
+        
+    @cherrypy.expose(alias='editExercise')
+    def edit_exercise_info(self, id, description, timeLimit, output):
+        self.verify_session()
+        db.edit_exercise(id, description, timeLimit, output)
+        raise cherrypy.HTTPRedirect("/admin/console")
+        
+    @cherrypy.expose
+    def edit_user(self, id):
+        self.verify_session()
+        open_data = db.get_user_exercises(id)
+        exercise_data = db.get_exercises()
+        user_data = db.get_users(id)
+        tmpl = env.get_template('admin_edit_user.html')
+        return tmpl.render(user=user_data, open_list=open_data, exercise_list=exercise_data)
+        
+    @cherrypy.expose(alias='editUser')
+    def edit_user_info(self, user_id, ex_id, add):
+        self.verify_session()
+        db.add_user_exercise(user_id, ex_id, add)
+        raise cherrypy.HTTPRedirect("/admin/console")
 
     def verify_session(self):
         if 'loggedInAdmin' not in cherrypy.session:
@@ -77,4 +120,14 @@ def initialize():
                             'server.socket_port': 8081,
                             'tools.sessions.on' : True
                            })
-    cherrypy.quickstart(index, '/')
+    conf = {
+         '/': {
+             'tools.sessions.on': True,
+             'tools.staticdir.root': os.path.abspath(os.getcwd())
+         },
+         '/static': {
+             'tools.staticdir.on': True,
+             'tools.staticdir.dir': './public'
+         }
+     }
+    cherrypy.quickstart(index, '/', conf)
